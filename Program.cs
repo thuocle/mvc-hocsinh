8c8d3e535b069a567fee6b9762025c68eb77630a
THUOC LE <anhtuan1990nxht@gmail.com>
2023-07-10 11:06:23 +0700

    v9

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using HocSinhMVC.View;
+namespace HocSinhMVC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HocSinhView view = new HocSinhView();
+            view.Menu();
+        }
+    }
+}
